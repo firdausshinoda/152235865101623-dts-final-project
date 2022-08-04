328b48e8e2cb40cccd5647867293181ee1e24aef
firdausns <firdausns44@gmail.com>
2022-08-04 10:02:32 +0700

    commit four

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAmiWD35dSHhBRU_xaQTyg09o0GKmeRrPQ",
-  authDomain: "agrocar-159704.firebaseapp.com",
-  databaseURL: "https://agrocar-159704.firebaseio.com",
-  projectId: "agrocar-159704",
-  storageBucket: "agrocar-159704.appspot.com",
-  messagingSenderId: "803767824331",
-  appId: "1:803767824331:web:986d5e920d36db1b40504e"
+  apiKey: "AIzaSyC6PlVYNxdlzW6eUfLdcWpCXb-uzhH57Mw",
+  authDomain: "tensorflow-f1345.firebaseapp.com",
+  projectId: "tensorflow-f1345",
+  storageBucket: "tensorflow-f1345.appspot.com",
+  messagingSenderId: "965051764557",
+  appId: "1:965051764557:web:f451f8ff0e07d038ddf104",
+  measurementId: "G-K4JXQ3W75J"
+
 };
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
