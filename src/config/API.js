34964cf0aeb541eee3a3cb054c0cab43b2cf50fa
firdausns <firdausns44@gmail.com>
2022-08-04 09:42:03 +0700

    commit seconds

--- src/config/API.js
+++ src/config/API.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: 'http://localhost/final_project/index.php?url=',
-    // baseURL: 'https://api.kingscoffeegroup.com?url=',
+    // baseURL: 'http://localhost/final_project/index.php?url=',
+    baseURL: 'https://api.kingscoffeegroup.com?url=',
 })
 
 export default API
